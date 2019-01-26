f6ea7245ee4d7dddbdfed73453b9e8dfa207b72d
Naritoshi Matsuo <naritoshi.matsuo@gmail.com>
2019-01-26 18:21:58 +0900

    b4 az"

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'b-bgvu7bi+c8os7kl-%zsc)+ol+=6&d6$1bog@-j$p8@d^s7mv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
